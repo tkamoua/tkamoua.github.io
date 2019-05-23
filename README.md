47d9170776a002211e4ba3fccbcac709f660d05d
tkamoua <tkamoua@umich.edu>
2019-05-23 10:46:37 -0400

    websitev2

--- README.md
+++
@@ -1,1 +0,0 @@
-# tkamoua.github.io
---
+++ scripts.js
@@ -0,0 +1,8 @@
+$('.menu').on('click', function(){
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+});
+$('.nav a').on('click', function (){
+    $('.menu').removeClass('active');
+    $('.overlay').removeClass('menu-open');
+});
