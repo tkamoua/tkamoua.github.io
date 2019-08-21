b353facdeccf77e38fe6ad7056c0341aab0302c5
tkamoua <tkamoua@umich.edu>
2019-08-21 01:07:51 -0400

    website v3

--- scripts.js
+++
@@ -1,8 +0,0 @@
-$('.menu').on('click', function(){
-    $(this).toggleClass('active');
-    $('.overlay').toggleClass('menu-open');
-});
-$('.nav a').on('click', function (){
-    $('.menu').removeClass('active');
-    $('.overlay').removeClass('menu-open');
-});
